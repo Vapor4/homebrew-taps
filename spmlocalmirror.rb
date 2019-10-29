bf982cf603bcb0ba12907bd20952ac81252898b1
君赏 <josercc@users.noreply.github.com>
2019-10-29 23:01:19 +0800

    Update spmlocalmirror.rb

--- spmlocalmirror.rb
+++ spmlocalmirror.rb
@@ -2,15 +2,14 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Spmlocalmirror < Formula
-  desc "Swift Package Manager本地镜像加速器"
-  homepage "SPMLocalMirror"
-  url "https://github.com/Vapor4/SPMLocalMirror/archive/0.1.3.tar.gz"
-  sha256 "f585372ec40ba9ac7dce2e995d86b2d1a8a2564549a3c2dcac717338a5163a6e"
-
-  # depends_on "cmake" => :build
-  def install
-    system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
-  end
+  desc "Swift Package Manager本地加速器"
+  homepage "https://github.com/Vapor4/SPMLocalMirror"
+  url "https://github.com/Vapor4/SPMLocalMirror.git", :tag => "0.1.4", :revision => "5c821170f713625ea8d1ed332fdf52885de3af3c"
+  head "https://github.com/Vapor4/SPMLocalMirror.git", :shallow => false
 
   depends_on :xcode => ["11.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
 end
